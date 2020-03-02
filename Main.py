aeba507fc2b3ac9bfeccc18166884fa83e99b9d5
Trocker43 <benkatin@gmail.com>
2020-03-02 15:11:49 -0800

    Merging

--- Main.py
+++ Main.py
@@ -2,4 +2,15 @@ from Problem1 import Part1
 from Problem2 import smallestMultiple
 
 #print(Part1(56))
-print(smallestMultiple(7, [3, 2]))
+parent = smallestMultiple(26147, [1,3])
+
+print(parent[0])
+print(parent[7844])
+print(parent[3399])
+print(parent[5569])
+print(parent[5786])
+print(parent[13652])
+print(parent[9209])
+print(parent[16609])
+print(parent[17349])
+print(parent[6964])
--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,5 @@
 import queue
+import numpy
 
 def smallestMultiple(k,digits):
     matrix = []
@@ -12,7 +13,67 @@ def smallestMultiple(k,digits):
             matrix[i][r] = (10 * i + digits[r]) % k
 
     matrix.insert(k+1, digits)
-    return matrix
+
+
+
+    return MinString(matrix,len(matrix) - 1)
 
 def MinString(DFA,startstate):
-    print()
+    queue = []
+
+
+    # array of indexes, 0 = not discovered, 1 = discovered.
+    discovered = [0] * len(DFA)
+    discovered[startstate] = 1
+
+    parent = [0] * len(DFA)
+
+    label = [""] * len(DFA)
+
+    queue.append(startstate)
+    overallcount = 0
+
+    while(len(queue) != 0):
+        curr = queue.pop(0)
+        count = 0
+        for transition in DFA[curr]:
+            next = transition
+            if next == 0:
+                parent[next] = curr
+                break
+            elif discovered[next] == 0:
+                discovered[next] = 1
+                parent[next] = curr
+
+
+                queue.append(next)
+                label[next] = str(DFA[0][count])
+                #What does this do? its in the pseudocode
+
+            count += 1
+            overallcount += 1
+
+    if(next != 0):
+        print("no valid solution")
+        return parent
+    else:
+        output = ""
+        while(next != len(DFA) - 1):
+            output += label[next]
+            next = parent[next]
+        print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
